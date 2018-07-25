b905d8a5165540a7c5bea179532ec8d5e320919a
Meghdad Hadidi <meghdad.1987@gmail.com>
2018-07-26 00:30:07 +0430

    Added body-parser

--- server/index.js
+++ server/index.js
@@ -1,8 +1,12 @@
 import express from 'express'
 import routes from './routes'
+import bodyParser from 'body-parser'
 
 const app = express()
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 4000
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
 
 app.use('/api', routes)
 
