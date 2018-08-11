0b7f1d3a773fd5d636049cfb173c6dedb3602a89
Meghdad Hadidi <meghdad.1987@gmail.com>
2018-08-11 14:02:24 +0430

    some tests

--- index.js
+++ index.js
@@ -19,6 +19,7 @@ app.use('/api', routes)
 // The "catchall" handler: for any request that doesn't
 // match one above, send back React's index.html file.
 app.get('*', (req, res, next) => {
+    console.log(__dirname);
     res.sendFile(path.join(__dirname + '/client/build/index.html'));
 });
 
