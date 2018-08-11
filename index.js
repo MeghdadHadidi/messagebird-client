1c5663cdcd3782092449e7c702ef2cc55c73adbb
Meghdad Hadidi <meghdad.1987@gmail.com>
2018-08-11 14:54:19 +0430

    finished saturday

--- index.js
+++ index.js
@@ -1,20 +1,34 @@
 import express from 'express'
 import routes from './routes'
 import bodyParser from 'body-parser'
+import compression from 'compression'
 import path from 'path'
+import morgan from 'morgan'
 import cors from 'cors'
 
-const app = express()
-const PORT = process.env.PORT || 4000
+const normalizePort = port => parseInt(port, 10)
+const PORT = normalizePort(process.env.PORT || 4000)
 
-// Serve static files from the React app
-app.use(express.static(path.resolve(__dirname, 'client/build')));
+const app = express()
+const dev = app.get('env') !== 'production'
+
+if(!dev){
+    app.disable('x-powered-by')
+    app.use(morgan('common'))
+    app.use(compression())
+    app.use(cors())
+}
+else{
+    app.use(morgan('dev'))
+}
 
 app.use(bodyParser.json())
-app.use(cors())
 app.use(bodyParser.urlencoded({ extended: false }))
 
 app.use('/api', routes)
+
+// Serve static files from the React app
+app.use(express.static(path.resolve(__dirname, 'client/build')));
 
 // The "catchall" handler: for any request that doesn't
 // match one above, send back React's index.html file.
